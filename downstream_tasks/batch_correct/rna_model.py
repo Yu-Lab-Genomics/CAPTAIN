7f5ac50815fcab8223ec60e3d7302b72969c2422
Ji Bo-Ya <40913179+iamjiboya@users.noreply.github.com>
2025-10-12 11:33:27 +0800

    Update rna_model.py

--- downstream_tasks/batch_correct/rna_model.py
+++ downstream_tasks/batch_correct/rna_model.py
@@ -126,7 +126,7 @@ def grad_reverse(x: torch.Tensor, lambd: float = 1.0) -> torch.Tensor:
 class Human_RNA_Embedding(nn.Module):
     def __init__(self):
         super().__init__()
-        gene2vec_weight = np.load('/home/jiboya/scBLIP/prior_know/final_human_prior_knwo.npy')
+        gene2vec_weight = np.load('./prior_know/final_human_prior_knwo.npy')
         gene2vec_weight = torch.from_numpy(gene2vec_weight)
         self.emb = nn.Embedding.from_pretrained(gene2vec_weight)
 
@@ -137,7 +137,7 @@ class Human_RNA_Embedding(nn.Module):
 class Mouse_RNA_Embedding(nn.Module):
     def __init__(self):
         super().__init__()
-        gene2vec_weight = np.load('/home/jiboya/scBLIP/prior_know/final_mouse_prior_knwo.npy')
+        gene2vec_weight = np.load('./prior_know/final_mouse_prior_knwo.npy')
         gene2vec_weight = torch.from_numpy(gene2vec_weight)
         self.emb = nn.Embedding.from_pretrained(gene2vec_weight)
 
